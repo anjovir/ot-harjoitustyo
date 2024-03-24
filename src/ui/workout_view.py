0db1a7937bb2eae3ef3694b427e331c042ca0587
Antti Virtanen <antti.b.virtanen@helsinki.fi>
2024-03-24 11:28:00 +0200

    First proto for saving workout programn to database

--- src/ui/workout_view.py
+++ src/ui/workout_view.py
@@ -1,4 +1,5 @@
 from tkinter import ttk, constants
+from wprogram_repository import WorkoutProgramRepository
 
 class WorkoutView:
 
@@ -7,25 +8,31 @@ class WorkoutView:
         self._handle_check_wod = handle_check_wod
         self._frame = None
         self._frame2 = None
+        self._frame3 = None
         
         self.rows = [0,1,2,3]
+        self.entries = []
 
         self._initialize()
     
     def pack(self):
         self._frame.pack(fill=constants.X)
         self._frame2.pack(fill=constants.X)
+        self._frame3.pack(fill=constants.X)
     
     def destroy(self):
         self._frame.destroy()
         self._frame2.destroy()
+        self._frame3.destroy()
     
     def create_new_row(self):
         current_row = len(self.rows)
         
-        for i in range(2):
-            entry = ttk.Entry(self._frame)
-            entry.grid(row=current_row, column=i)
+        self.entry1 = ttk.Entry(self._frame)
+        self.entry1.grid(row=current_row, column=0)
+        self.entry2 = ttk.Entry(self._frame)
+        self.entry2.grid(row=current_row, column=1)
+        self.entries.append([self.entry1, self.entry2])
         
         wod_button = ttk.Button(
             master=self._frame,
@@ -36,17 +43,29 @@ class WorkoutView:
         wod_button.grid(row=current_row,column=2)
         
         self.rows.append(len(self.rows))
+        print(self.entries)
     
+    def save(self):
+        rep = WorkoutProgramRepository()
+        for entry in self.entries:
+            wlabel = entry[0].get()
+            weekday = entry[1].get()
+            print("Tulostus:", wlabel, weekday)
+            rep.write("Workout program",wlabel, weekday)
+
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
         self._frame2 = ttk.Frame(master=self._root)
+        self._frame3 = ttk.Frame(master=self._root)
         label = ttk.Label(master=self._frame, text="Workout program")
 
-        workout_label = ttk.Label(master=self._frame, text="Workout label")
+        self.workout_label = ttk.Label(master=self._frame, text="Workout label")
         weekday_label = ttk.Label(master=self._frame, text="Weekday")
 
-        workout_label_entry = ttk.Entry(master=self._frame)
-        weekday_label_entry = ttk.Entry(master=self._frame)
+        self.workout_label_entry = ttk.Entry(master=self._frame)
+        self.weekday_label_entry = ttk.Entry(master=self._frame)
+
+        self.entries.append([self.workout_label_entry, self.weekday_label_entry])
 
         wod_button = ttk.Button(
             master=self._frame,
@@ -60,13 +79,20 @@ class WorkoutView:
             command=self.create_new_row
         )
 
+        save_button = ttk.Button(
+            master=self._frame3,
+            text="Save",
+            command=self.save
+        )
+
         label.grid(row=0, column=0)
-        workout_label.grid(row=2, column=0)
+        self.workout_label.grid(row=2, column=0)
         weekday_label.grid(row=2, column=1)
-        workout_label_entry.grid(row=3, column=0)
-        weekday_label_entry.grid(row=3, column=1)
+        self.workout_label_entry.grid(row=3, column=0)
+        self.weekday_label_entry.grid(row=3, column=1)
         wod_button.grid(row=3, column=2)
         
-        
         add_new_wod_button.grid(row=4, column=0)
+
+        save_button.grid(row=5, column=0)
         
--- src/wprogram_repository.py
+++ src/wprogram_repository.py
@@ -3,8 +3,8 @@ from database_connection import get_database_connection
 import sqlite3
 
 class WorkoutProgramRepository:
-    def __init__(self, connection):
-        self._connection = connection
+    def __init__(self):
+        self._connection = get_database_connection()
     
     def find_all(self):
         cursor = self._connection.cursor()
@@ -15,5 +15,31 @@ class WorkoutProgramRepository:
 
         return [WorkoutProgram(row["wprogram_name"], row["wod_name"], row["weekday"]) for row in rows]
 
-workout_program_repository = WorkoutProgramRepository(get_database_connection())
-workout_programs =workout_program_repository.find_all()
+
+    def save_workout_program(self, workout_program):
+        """Saves the workout program to the db
+
+        Args:
+            workout_program: saved as WorkoutProgram object
+
+        Returns:
+            Saves the workout program as WorkoutProgram object
+        """
+
+        workout_programs = self.find_all()
+        workout_programs.append(workout_program)
+
+    
+    def write(self, wprogram_name, wod_name, weekday):
+        cursor = self._connection.cursor()
+
+        cursor.execute("""
+            INSERT INTO workout_program (wprogram_name, wod_name, weekday)
+            VALUES (?, ?, ?)""", 
+            (wprogram_name, wod_name, weekday))
+
+        self._connection.commit()
+    
+    
+#workout_program_repository = WorkoutProgramRepository()
+#workout_programs =workout_program_repository.find_all()
