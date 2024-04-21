dba17a13f129cbd8b522e6b994cabf47b1827877
Antti Virtanen <antti.b.virtanen@helsinki.fi>
2024-04-21 15:59:34 +0300

    Fixed user tests

--- src/tests/user_test.py
+++ src/tests/user_test.py
@@ -4,22 +4,22 @@ from entities.user import User
 
 class TestUser(unittest.TestCase):
     def setUp(self):
-        self._user = User("testihenkilö", "testisalasana")
+        self._user = User("testihenkilö", "testisalasana",1)
 
     def test_constructor_works(self):
         user = User("testihenkilö", "testisalasana")
-        self.assertEqual(self._user.username, user.username)
-        self.assertEqual(self._user.password, user.password)
+        self.assertEqual(self._user.username(), user.username())
+        self.assertEqual(self._user.password(), user.password())
 
     def test_username_method_returns_the_right_username(self):
-        usernaname = self._user.username
+        usernaname = self._user.username()
         self.assertEqual(usernaname, "testihenkilö")
 
     def test_password_method_returns_the_right_password(self):
-        password = self._user.password
+        password = self._user.password()
         self.assertEqual(password, "testisalasana")
 
     def test_to_string_operator(self):
         answer = self._user.__str__()
         self.assertEqual(
-            answer, "username: testihenkilö, password: testisalasana")
+            answer, "username: testihenkilö, password: testisalasana, user_id: 1")
