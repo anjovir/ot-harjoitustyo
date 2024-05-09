68516a2ef20e4b04897d9828f367654f6826f52e
Antti Virtanen <antti.b.virtanen@helsinki.fi>
2024-05-09 13:16:39 +0300

    Deleted unused imports

--- src/config.py
+++ src/config.py
@@ -6,11 +6,9 @@ dirname = os.path.dirname(__file__)
 test = os.getenv("DATABASE FILENAME")
 
 try:
-    load_dotenv()
+    load_dotenv(dotenv_path=os.path.join(dirname, "..", ".env"))
 except FileNotFoundError:
     pass
 
 DATABASE_FILENAME = os.getenv("DATABASE FILENAME") or "database.sqlite"
-print(DATABASE_FILENAME)
 DATABASE_FILE_PATH = os.path.join(dirname, "..", "data", DATABASE_FILENAME)
-print(DATABASE_FILE_PATH)
--- src/database_connection.py
+++ src/database_connection.py
@@ -1,8 +1,6 @@
 import sqlite3
 from config import DATABASE_FILE_PATH
-from config import DATABASE_FILENAME
 
-print(DATABASE_FILENAME)
 connection = sqlite3.connect(DATABASE_FILE_PATH)
 connection.row_factory = sqlite3.Row
 connection.execute("PRAGMA foreign_keys = ON")
